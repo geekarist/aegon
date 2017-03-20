99dbdbb43722faeaac5f2481ea7baa696961410d
cpcp <christophe.pele@gmail.com>
2017-03-20 08:30:44 +0100

    Initialize time

--- app/src/main/java/me/cpele/aegon/MainActivity.java
+++ app/src/main/java/me/cpele/aegon/MainActivity.java
@@ -3,6 +3,7 @@ package me.cpele.aegon;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +12,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        initTime();
+    }
+
+    private void initTime() {
+        TextView timeTextView = (TextView) findViewById(R.id.main_tv_time);
+
+        int hour = 0, min = 0, sec = 0;
+        timeTextView.setText(getString(R.string.main_time, hour, min, sec));
     }
 }
