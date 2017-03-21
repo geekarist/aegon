7be170eff7b1f40506cb30ce8af85b35b6a7ad16
cpcp <christophe.pele@gmail.com>
2017-03-21 08:28:58 +0100

    Let the user pick a time

--- app/src/main/java/me/cpele/aegon/MainActivity.java
+++ app/src/main/java/me/cpele/aegon/MainActivity.java
@@ -1,9 +1,12 @@
 package me.cpele.aegon;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends Activity {
 
@@ -13,13 +16,25 @@ public class MainActivity extends Activity {
 
         setContentView(R.layout.activity_main);
 
-        initTime();
+        initTime(0, 0);
+
+        findViewById(R.id.main_tv_time).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        initTime(hourOfDay, minute);
+                    }
+                }, 0, 0, true).show();
+            }
+        });
     }
 
-    private void initTime() {
+    private void initTime(int hour, int min) {
         TextView timeTextView = (TextView) findViewById(R.id.main_tv_time);
 
-        int hour = 0, min = 0, sec = 0;
+        int sec = 0;
         timeTextView.setText(getString(R.string.main_time, hour, min, sec));
     }
 }
