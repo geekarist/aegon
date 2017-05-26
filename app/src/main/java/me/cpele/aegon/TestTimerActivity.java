5686081036153c54180fbd93d1beb9940e5ca48c
cpcp <christophe.pele@gmail.com>
2017-05-26 08:54:49 +0200

    Make TestTimerActivity implement Listener

--- app/src/main/java/me/cpele/aegon/TestTimerActivity.java
+++ app/src/main/java/me/cpele/aegon/TestTimerActivity.java
@@ -3,13 +3,32 @@ package me.cpele.aegon;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.widget.Toast;
 
-public class TestTimerActivity extends FragmentActivity {
+public class TestTimerActivity extends FragmentActivity implements TimerFragment.Listener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_test_timer);
+
+        TimerFragment timerFragment = (TimerFragment) getSupportFragmentManager().findFragmentById(R.id.test_timer_fr_timer);
+        timerFragment.showTimePicker();
+    }
+
+    @Override
+    public void over() {
+        Toast.makeText(this, "It's over", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void cancel() {
+        Toast.makeText(this, "Cancel", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void makeNotification(String timeStr) {
+        Toast.makeText(this, "Make a notification", Toast.LENGTH_SHORT).show();
     }
 }
