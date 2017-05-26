eaebc7067bcb0673bc5771c65bc49d2fe89a2601
cpcp <christophe.pele@gmail.com>
2017-05-26 07:56:38 +0200

    Extract countdown timer to fragment

--- app/src/main/java/me/cpele/aegon/MainActivity.java
+++ app/src/main/java/me/cpele/aegon/MainActivity.java
@@ -5,7 +5,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.annotation.NonNull;
@@ -14,22 +13,19 @@ import android.support.v4.app.FragmentActivity;
 import android.view.WindowManager;
 import android.widget.TextView;
 
-import com.codetroopers.betterpickers.hmspicker.HmsPickerBuilder;
-
 import java.util.Timer;
 import java.util.TimerTask;
 
 import static java.util.concurrent.TimeUnit.HOURS;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.MINUTES;
-import static java.util.concurrent.TimeUnit.SECONDS;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements TimerFragment.Listener {
 
-    private static final long HALF_SEC = 500;
     private static final int NOTIFICATION_ID = 0;
     public static final String KEY_TIME_OF_ARRIVAL = "TIME_OF_ARRIVAL";
     public static final String KEY_START_TIME = "START_TIME";
+    private static final long HALF_SEC = 500;
 
     /**
      * Absolute time OF arrival: this is different from the time TO arrival
@@ -37,11 +33,11 @@ public class MainActivity extends FragmentActivity {
     private long mTimeOfArrival;
     private long mStartTime;
 
-    private TextView mTimeTextView;
-    private CountDownTimer mTimer;
     private Timer mStopwatch;
     private NotificationManager mNotificationManager;
     private boolean mBackground = true;
+    private TimerFragment mTimerFragment;
+    private TextView mStopwatchTextView;
 
     // region Android lifecycle
 
@@ -51,16 +47,15 @@ public class MainActivity extends FragmentActivity {
 
         setContentView(R.layout.activity_main);
 
-        mTimeTextView = (TextView) findViewById(R.id.main_tv_time);
-
         mNotificationManager = getSystemService(NotificationManager.class);
-
-        mTimeTextView.setOnClickListener(v -> showTimePicker());
 
         if (savedInstanceState != null) {
             restoreStateFrom(savedInstanceState);
             startTimer();
-        } else showTimePicker();
+        }
+        else mTimerFragment.showTimePicker();
+
+        mStopwatchTextView = (TextView) findViewById(R.id.main_tv_stopwatch);
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
@@ -87,12 +82,56 @@ public class MainActivity extends FragmentActivity {
     @Override
     protected void onDestroy() {
         cancelNotification();
-        if (mTimer != null) mTimer.cancel();
+        mTimerFragment.cancel();
         if (mStopwatch != null) mStopwatch.cancel();
         super.onDestroy();
     }
 
     // endregion
+
+    // region TimerFragment.Listener
+
+    @Override
+    public void over() {
+        mStopwatch = new Timer();
+        mStopwatch.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new Handler(Looper.getMainLooper()).post(() -> updateStopwatchView());
+            }
+        }, 0, HALF_SEC);
+    }
+
+    @Override
+    public void cancel() {
+        if (mStopwatch != null) mStopwatch.cancel();
+    }
+
+    @Override
+    public void makeNotification(String timeStr) {
+
+        Intent intent = new Intent(this, getClass());
+        Notification notification = new Notification.Builder(this)
+                .setContentIntent(PendingIntent.getActivity(this, 0, intent, 0))
+                .setSmallIcon(R.drawable.ic_hourglass_empty_black_24dp)
+                .setContentTitle(getString(R.string.main_notification_text))
+                .setContentText(getString(R.string.main_notification_sub_text))
+                .setSubText(timeStr)
+                .build();
+        mNotificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    // endregion
+
+    private void startTimer() {
+
+        mTimerFragment = TimerFragment.newInstance(mStartTime, mTimeOfArrival);
+        mTimerFragment.start();
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.main_fr_timer_container, mTimerFragment)
+                .commit();
+    }
 
     private void saveStateTo(@NonNull Bundle outState) {
         outState.putLong(KEY_TIME_OF_ARRIVAL, mTimeOfArrival);
@@ -102,52 +141,6 @@ public class MainActivity extends FragmentActivity {
     private void restoreStateFrom(@NonNull Bundle savedInstanceState) {
         mTimeOfArrival = savedInstanceState.getLong(KEY_TIME_OF_ARRIVAL);
         mStartTime = savedInstanceState.getLong(KEY_START_TIME);
-    }
-
-    private void showTimePicker() {
-        new HmsPickerBuilder().addHmsPickerDialogHandler(
-                (reference, isNegative, hours, minutes, seconds) -> {
-                    if (mTimer != null) mTimer.cancel();
-                    if (mStopwatch != null) mStopwatch.cancel();
-                    mTimeOfArrival = 1000 + System.currentTimeMillis()
-                            + HOURS.toMillis(hours)
-                            + MINUTES.toMillis(minutes)
-                            + SECONDS.toMillis(seconds);
-                    mStartTime = System.currentTimeMillis();
-                    startTimer();
-                })
-                .setFragmentManager(getSupportFragmentManager())
-                .setStyleResId(R.style.BetterPickersDialogFragment)
-                .show();
-    }
-
-    private void startTimer() {
-
-        long timeToArrival = mTimeOfArrival - System.currentTimeMillis();
-
-        mTimer = new CountDownTimer(timeToArrival, HALF_SEC) {
-
-            @Override
-            public void onTick(long millisUntilFinished) {
-                updateEtaView();
-            }
-
-            @Override
-            public void onFinish() {
-                switchToStopWatch();
-                if (mTimer != null) mTimer.cancel();
-            }
-        }.start();
-    }
-
-    private void switchToStopWatch() {
-        mStopwatch = new Timer();
-        mStopwatch.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                new Handler(Looper.getMainLooper()).post(() -> updateStopwatchView());
-            }
-        }, 0, HALF_SEC);
     }
 
     private void updateStopwatchView() {
@@ -160,52 +153,9 @@ public class MainActivity extends FragmentActivity {
         long totalMin = MILLISECONDS.toMinutes(total) - HOURS.toMinutes(totalHour);
         long totalSec = MILLISECONDS.toSeconds(total) - HOURS.toSeconds(totalHour) - MINUTES.toSeconds(totalMin);
         String timeStr = getString(R.string.main_time_vs_total, hour, min, sec, totalHour, totalMin, totalSec);
-        mTimeTextView.setText(timeStr);
-        mTimeTextView.setTextColor(getResources().getColor(R.color.bpDarker_red, null));
+        mStopwatchTextView.setText(timeStr);
+        mStopwatchTextView.setTextColor(getResources().getColor(R.color.bpDarker_red, null));
         if (mBackground) makeNotification(timeStr);
-    }
-
-    private void updateEtaView() {
-
-        long hour = 0;
-        long min = 0;
-        long sec = 0;
-
-        long totalHour = 0;
-        long totalMin = 0;
-        long totalSec = 0;
-
-        if (mTimeOfArrival != 0) {
-
-            long timeToArrival = mTimeOfArrival - System.currentTimeMillis();
-            hour = MILLISECONDS.toHours(timeToArrival);
-            min = MILLISECONDS.toMinutes(timeToArrival) - HOURS.toMinutes(hour);
-            sec = MILLISECONDS.toSeconds(timeToArrival) - MINUTES.toSeconds(min) - HOURS.toSeconds(hour);
-
-            long total = mTimeOfArrival - mStartTime;
-            totalHour = MILLISECONDS.toHours(total);
-            totalMin = MILLISECONDS.toMinutes(total) - HOURS.toMinutes(totalHour);
-            totalSec = MILLISECONDS.toSeconds(total) - HOURS.toSeconds(totalHour) - MINUTES.toSeconds(totalMin);
-        }
-
-        String timeStr = getString(R.string.main_time_vs_total, hour, min, sec, totalHour, totalMin, totalSec);
-        mTimeTextView.setText(timeStr);
-        mTimeTextView.setTextColor(getResources().getColor(R.color.bpblack, null));
-
-        if (mBackground) makeNotification(timeStr);
-    }
-
-    private void makeNotification(String timeStr) {
-
-        Intent intent = new Intent(this, getClass());
-        Notification notification = new Notification.Builder(this)
-                .setContentIntent(PendingIntent.getActivity(this, 0, intent, 0))
-                .setSmallIcon(R.drawable.ic_hourglass_empty_black_24dp)
-                .setContentTitle(getString(R.string.main_notification_text))
-                .setContentText(getString(R.string.main_notification_sub_text))
-                .setSubText(timeStr)
-                .build();
-        mNotificationManager.notify(NOTIFICATION_ID, notification);
     }
 
     private void cancelNotification() {
---
+++ app/src/main/java/me/cpele/aegon/TimerFragment.java
@@ -0,0 +1,158 @@
+package me.cpele.aegon;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.codetroopers.betterpickers.hmspicker.HmsPickerBuilder;
+
+import static java.util.concurrent.TimeUnit.HOURS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class TimerFragment extends Fragment {
+
+    private static final long HALF_SEC = 500;
+    public static final String KEY_TIME_OF_ARRIVAL = "TIME_OF_ARRIVAL";
+    public static final String KEY_START_TIME = "START_TIME";
+
+    private long mTimeOfArrival;
+    private long mStartTime;
+
+    private Listener mListener;
+    private CountDownTimer mTimer;
+    private boolean mBackground;
+    private TextView mTimeTextView;
+
+    static TimerFragment newInstance(long startTime, long timeOfArrival) {
+        TimerFragment timerFragment = new TimerFragment();
+        Bundle bundle = new Bundle();
+        bundle.putLong(KEY_START_TIME, startTime);
+        bundle.putLong(KEY_TIME_OF_ARRIVAL, timeOfArrival);
+        timerFragment.setArguments(bundle);
+        return timerFragment;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        mBackground = false;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        mBackground = true;
+    }
+
+    public void start() {
+
+        long timeToArrival = mTimeOfArrival - System.currentTimeMillis();
+
+        mTimer = new CountDownTimer(timeToArrival, HALF_SEC) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateEtaView();
+            }
+
+            @Override
+            public void onFinish() {
+                mListener.over();
+                if (mTimer != null) mTimer.cancel();
+            }
+        }.start();
+    }
+
+    public void cancel() {
+        if (mTimer != null) mTimer.cancel();
+    }
+
+    interface Listener {
+
+        void over();
+
+        void cancel();
+
+        void makeNotification(String timeStr);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_timer, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mTimeTextView = (TextView) view.findViewById(R.id.main_tv_time);
+        mTimeTextView.setOnClickListener(v -> showTimePicker());
+    }
+
+    void showTimePicker() {
+        new HmsPickerBuilder().addHmsPickerDialogHandler(
+                (reference, isNegative, hours, minutes, seconds) -> {
+                    if (mTimer != null) mTimer.cancel();
+                    mListener.cancel();
+                    mTimeOfArrival = 1000 + System.currentTimeMillis()
+                            + HOURS.toMillis(hours)
+                            + MINUTES.toMillis(minutes)
+                            + SECONDS.toMillis(seconds);
+                    mStartTime = System.currentTimeMillis();
+                    start();
+                })
+                .setFragmentManager(getActivity().getSupportFragmentManager())
+                .setStyleResId(R.style.BetterPickersDialogFragment)
+                .show();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        
+        if (context instanceof Listener) mListener = (Listener) context;
+    }
+
+    private void updateEtaView() {
+
+        long hour = 0;
+        long min = 0;
+        long sec = 0;
+
+        long totalHour = 0;
+        long totalMin = 0;
+        long totalSec = 0;
+
+        if (mTimeOfArrival != 0) {
+
+            long timeToArrival = mTimeOfArrival - System.currentTimeMillis();
+            hour = MILLISECONDS.toHours(timeToArrival);
+            min = MILLISECONDS.toMinutes(timeToArrival) - HOURS.toMinutes(hour);
+            sec = MILLISECONDS.toSeconds(timeToArrival) - MINUTES.toSeconds(min) - HOURS.toSeconds(hour);
+
+            long total = mTimeOfArrival - mStartTime;
+            totalHour = MILLISECONDS.toHours(total);
+            totalMin = MILLISECONDS.toMinutes(total) - HOURS.toMinutes(totalHour);
+            totalSec = MILLISECONDS.toSeconds(total) - HOURS.toSeconds(totalHour) - MINUTES.toSeconds(totalMin);
+        }
+
+        String timeStr = getString(R.string.main_time_vs_total, hour, min, sec, totalHour, totalMin, totalSec);
+        mTimeTextView.setText(timeStr);
+        mTimeTextView.setTextColor(getResources().getColor(R.color.bpblack, null));
+
+        if (mBackground) mListener.makeNotification(timeStr);
+    }
+}
