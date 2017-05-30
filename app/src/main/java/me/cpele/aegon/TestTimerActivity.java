ca425a7c2f614e7d93032ad4e90f4cc2a592b41c
cpcp <christophe.pele@gmail.com>
2017-05-30 20:30:48 +0200

    Don't use findFragmentByTag

--- app/src/main/java/me/cpele/aegon/TestTimerActivity.java
+++ app/src/main/java/me/cpele/aegon/TestTimerActivity.java
@@ -3,6 +3,7 @@ package me.cpele.aegon;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.widget.Toast;
 
 public class TestTimerActivity extends FragmentActivity implements TimerFragment.Listener {
@@ -13,7 +14,8 @@ public class TestTimerActivity extends FragmentActivity implements TimerFragment
 
         setContentView(R.layout.activity_test_timer);
 
-        final TimerFragment timerFragment = TimerFragment.find(getSupportFragmentManager(), R.id.test_timer_fr_timer);
+        FragmentManager manager = getSupportFragmentManager();
+        final TimerFragment timerFragment = (TimerFragment) manager.findFragmentById(R.id.test_timer_fr_timer);
         findViewById(R.id.test_timer_bt_cancel).setOnClickListener(v -> timerFragment.cancel());
     }
 
--- app/src/main/java/me/cpele/aegon/TimerFragment.java
+++ app/src/main/java/me/cpele/aegon/TimerFragment.java
@@ -6,7 +6,6 @@ import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -117,14 +116,6 @@ public class TimerFragment extends Fragment {
 
     public void cancel() {
         if (mTimer != null) mTimer.cancel();
-    }
-
-    public static TimerFragment find(FragmentManager manager, int id) {
-        TimerFragment fragment = (TimerFragment) manager.findFragmentByTag("TIMER");
-        if (fragment == null) {
-            fragment = (TimerFragment) manager.findFragmentById(id);
-        }
-        return fragment;
     }
 
     interface Listener {
