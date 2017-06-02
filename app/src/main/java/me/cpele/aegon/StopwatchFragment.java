32896bd75b06b3259f8dc72430eb5effcff36df9
cpcp <christophe.pele@gmail.com>
2017-06-02 07:59:54 +0200

    WIP: extract stopwatch fragment logic

--- app/src/main/java/me/cpele/aegon/StopwatchFragment.java
+++ app/src/main/java/me/cpele/aegon/StopwatchFragment.java
@@ -1,17 +1,66 @@
 package me.cpele.aegon;
 
 import android.app.Fragment;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static java.util.concurrent.TimeUnit.HOURS;
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.MINUTES;
 
 public class StopwatchFragment extends Fragment {
+
+    private static final long HALF_SEC = 500;
+
+    private Timer mStopwatch;
+
+    private TextView mTimeTextView;
+
+    private long mTimeOfArrival;
+    private long mStartTime;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_stopwatch, container, false);
+    }
+
+    private void switchToStopWatch() {
+        mStopwatch = new Timer();
+        mStopwatch.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new Handler(Looper.getMainLooper()).post(() -> updateStopwatchView());
+            }
+        }, 0, HALF_SEC);
+    }
+
+    private void updateStopwatchView() {
+        long elapsedTime = System.currentTimeMillis() - mStartTime;
+        long hour = MILLISECONDS.toHours(elapsedTime);
+        long min = MILLISECONDS.toMinutes(elapsedTime) - HOURS.toMinutes(hour);
+        long sec = MILLISECONDS.toSeconds(elapsedTime) - MINUTES.toSeconds(min) - HOURS.toSeconds(hour);
+        long total = mTimeOfArrival - mStartTime;
+        long totalHour = MILLISECONDS.toHours(total);
+        long totalMin = MILLISECONDS.toMinutes(total) - HOURS.toMinutes(totalHour);
+        long totalSec = MILLISECONDS.toSeconds(total) - HOURS.toSeconds(totalHour) - MINUTES.toSeconds(totalMin);
+        String timeStr = getString(R.string.main_time_vs_total, hour, min, sec, totalHour, totalMin, totalSec);
+        mTimeTextView.setText(timeStr);
+        mTimeTextView.setTextColor(getResources().getColor(R.color.bpDarker_red, null));
+
+        mOnTickListener.accept(mBackground, timeStr);
     }
 }
--- app/src/main/java/me/cpele/aegon/TestStopwatchActivity.java
+++ app/src/main/java/me/cpele/aegon/TestStopwatchActivity.java
@@ -1,15 +1,41 @@
 package me.cpele.aegon;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 
 public class TestStopwatchActivity extends FragmentActivity {
 
+    private NotificationManager mNotificationManager;
+    private boolean mBackground = true;
+
+    private static final int NOTIFICATION_ID = 0;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_test_stopwatch);
+    }
+
+    private void makeNotification(String timeStr) {
+
+        Intent intent = new Intent(this, getClass());
+        Notification notification = new Notification.Builder(this)
+                .setContentIntent(PendingIntent.getActivity(this, 0, intent, 0))
+                .setSmallIcon(R.drawable.ic_hourglass_empty_black_24dp)
+                .setContentTitle(getString(R.string.main_notification_text))
+                .setContentText(getString(R.string.main_notification_sub_text))
+                .setSubText(timeStr)
+                .build();
+        mNotificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    private void cancelNotification() {
+        mNotificationManager.cancel(NOTIFICATION_ID);
     }
 }
